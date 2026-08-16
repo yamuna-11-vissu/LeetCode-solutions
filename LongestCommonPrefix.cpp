ca5f00754a79e2069a53ff5ee3d2a0fa32f36d2d
Yamuna <pasamyamunasrisai@gmail.com>
2026-08-16 23:11:33 +0530

    adding longest common prefix

---
+++ LongestCommonPrefix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        string ans = "";
+
+        for (int i = 0; i < strs[0].size(); i++) {
+
+            char ch = strs[0][i];
+
+            for (int j = 1; j < strs.size(); j++) {
+
+                if (i >= strs[j].size() || strs[j][i] != ch)
+                    return ans;
+            }
+
+            ans += ch;
+        }
+
+        return ans;
+    }
+};
